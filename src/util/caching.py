8aa0e46bde8b4c98ffbb892090388b7b12382ed4
Alexander Grote <grote@fzi.de>
2024-03-10 17:32:46 +0100

    updating caching

--- src/util/caching.py
+++ src/util/caching.py
@@ -1,8 +1,44 @@
+import pandas as pd
 import pickle
 import hashlib
+import joblib
+from pydantic import BaseModel, field_validator
 from pathlib import Path
+from typing import Any, Optional
 
 from src.util.constants import Directory
+from src.util.filepath_converter import normalize_to_posix_path
+
+
+class PickleCacheHandler(BaseModel):
+
+    filepath: Path
+
+    @field_validator("filepath")
+    def _set_directory(cls, v):
+        return Directory.OUTPUT_DIR / v
+
+    def read(self) -> Optional[Any]: 
+
+        if not self.filepath.exists():
+            return None
+        
+        with open(self.filepath, 'rb') as cachehandle:
+            return pickle.load(cachehandle)
+ 
+    def write(self, obj: Any):
+
+        if not self.filepath.exists():
+            self.filepath.parent.mkdir(exist_ok=True, parents=True)
+
+        # write to cache file
+        with open(self.filepath, 'wb') as cachehandle:
+            pickle.dump(obj, cachehandle)
+
+
+def hash_dataframe(data: pd.DataFrame) -> str:
+    return joblib.hash(data)
+
 
 
 def pickle_cache(ignore_caching: bool, cachedir: str = 'tmp'):
@@ -16,11 +52,7 @@ def pickle_cache(ignore_caching: bool, cachedir: str = 'tmp'):
 
             if ignore_caching:
                 return fn(*args, **kwargs)
-
-            # init directory for caching
-            cache_dir = Directory.OUTPUT_DIR / cachedir
-            cache_dir.mkdir(exist_ok=True, parents=True)
-
+            
             # create filename from kwargs
             filename_components = [fn.__code__.co_filename, fn.__name__]
             filename_components += [str(arg) for arg in args]
@@ -29,19 +61,22 @@ def pickle_cache(ignore_caching: bool, cachedir: str = 'tmp'):
 
             # create hash for shorter filenames
             hash_object = hashlib.sha1(str.encode(filename_verbose))
-            filename_pickle = cache_dir / f"{hash_object.hexdigest()}.pickle"
+            filename_pickle = Path(cachedir) / f"{hash_object.hexdigest()}.pickle"
 
-            # if cache exists -> load it and return its content
-            if Path(filename_pickle).exists():
-                with open(filename_pickle, 'rb') as cachehandle:
-                    return pickle.load(cachehandle)
+            cache_handler = PickleCacheHandler(
+                filepath=filename_pickle
+            )
+
+            res = cache_handler.read()
+
+            if res is not None:
+                return res
 
             # execute the function with all arguments passed
             res = fn(*args, **kwargs)
 
             # write to cache file
-            with open(filename_pickle, 'wb') as cachehandle:
-                pickle.dump(res, cachehandle)
+            cache_handler.write(obj=res)
 
             return res
 
