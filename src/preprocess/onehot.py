491a5e5f478cfb8a15aae052f78f2e9cba1e3465
Alexander Grote <grote@fzi.de>
2024-03-12 14:55:33 +0100

    fixing naming issue in onehot script

--- src/preprocess/onehot.py
+++ src/preprocess/onehot.py
@@ -7,24 +7,25 @@ from src.preprocess.base import BasePreprocessor
 class OneHotEncoder(BaseModel, BasePreprocessor):
 
     id_column: str
-    feature_column: str  
+    feature_column: str
+    target_column: str  
 
     def execute(self, *, event: pd.DataFrame, **kwargs) -> dict:
 
         data_copy = event.copy()
 
         # one hot encode data
-        df_pivot = data_copy[['id', 'value']].drop_duplicates().pivot_table(index='id', columns='value', aggfunc='size')
+        df_pivot = data_copy[[self.id_column, self.feature_column]].drop_duplicates().pivot_table(index=self.id_column, columns=self.feature_column, aggfunc='size')
         df_pivot = ~df_pivot.isna()
         df_pivot = df_pivot.astype(int)
-        df_pivot.index.name = 'id'
+        df_pivot.index.name = self.id_column
         df_pivot.reset_index(inplace=True)
 
         # merge maleware back to data
-        df_maleware = data_copy[['id', 'malware']].drop_duplicates()
-        df_pivot = df_pivot.merge(df_maleware, left_on='id', right_on='id', how='inner')
+        df_maleware = data_copy[[self.id_column, self.target_column]].drop_duplicates()
+        df_pivot = df_pivot.merge(df_maleware, left_on=self.id_column, right_on=self.id_column, how='inner')
 
-        assert df_pivot.shape[0] == data_copy['id'].nunique()
+        assert df_pivot.shape[0] == data_copy[self.id_column].nunique()
 
         kwargs['data'] = df_pivot
         
