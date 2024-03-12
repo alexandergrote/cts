5daaab49aff7875f948620c0e1a9a9d593dcd60b
Alexander Grote <grote@fzi.de>
2024-03-12 14:57:44 +0100

    updating yaml and bat files

---
+++ bat/classification/synthetic__baseline.bat
@@ -0,0 +1,6 @@
+python src\main.py ^
+fetch_data=synthetic ^
+preprocess=synthetic__baseline ^
+model=xgb ^
+evaluation=ml.yaml ^
+export=mlflow.yaml export.params.experiment_name="clf__synthetic__baseline"
