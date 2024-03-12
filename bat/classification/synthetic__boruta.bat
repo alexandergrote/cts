ce0e19596d2758e4de3e33e1589954438ed3b933
Alexander Grote <grote@fzi.de>
2024-03-12 18:40:54 +0100

    adapting for multi run batfiles

--- bat/classification/synthetic__boruta.bat
+++ bat/classification/synthetic__boruta.bat
@@ -1,6 +1,8 @@
+echo --- model %2 and random_state: %1 ---
+
 python src\main.py ^
 fetch_data=synthetic ^
-preprocess=synthetic__boruta ^
-model=xgb ^
+preprocess=synthetic__boruta preprocess.params.steps.3.params.random_state=%1 ^
+model=%2 ^
 evaluation=ml.yaml ^
 export=mlflow.yaml export.params.experiment_name="clf__synthetic__boruta"
--- bat/classification/synthetic__cts.bat
+++ bat/classification/synthetic__cts.bat
@@ -1,6 +1,8 @@
+echo --- model %2 and random_state: %1 ---
+
 python src\main.py ^
 fetch_data=synthetic ^
-preprocess=synthetic ^
-model=xgb ^
+preprocess=synthetic preprocess.params.steps.2.params.random_state=%1 ^
+model=%2 ^
 evaluation=ml.yaml ^
 export=mlflow.yaml export.params.experiment_name="clf__synthetic__cts"
