d29f98a0883a704f55153e98b89d689a84306bc4
Alexander Grote <grote@fzi.de>
2024-03-15 09:07:24 +0100

    renaming cts yaml files

--- bat/corr_analysis/synthetic__run_experiment.bat
+++ bat/corr_analysis/synthetic__run_experiment.bat
@@ -1,6 +1,6 @@
 python src\main.py ^
 fetch_data=synthetic ^
-preprocess=synthetic ^
+preprocess=synthetic__cts ^
 model=xgb ^
 evaluation=ml.yaml ^
 export=mlflow.yaml export.params.experiment_name="synthetic__feat_selection"
