d157f1e6d427632411c63bbec1be903c7e05de5c
Alexander Grote <grote@fzi.de>
2024-03-19 21:57:44 +0100

    adding special case of theilu for customer churn

--- bat/single_file.bat
+++ bat/single_file.bat
@@ -4,10 +4,16 @@ REM example call: my\path\single_file.bat malware baseline xgb 0 null
 
 echo --- case %1, feature_selection %2, model %3, random_state %4 and features %5 ---
 
+set theilu= 
+
+if %1==churn if %2==cts (
+    set theilu=preprocess.params.extractor.params.corr_threshold=0
+) 
+
 python src\main.py ^
 constants=%1 ^
 fetch_data=%1 ^
-preprocess=%2 preprocess.params.selector.params.n_features=%5 ^
+preprocess=%2 preprocess.params.selector.params.n_features=%5 %theilu% ^
 train_test_split=stratified train_test_split.params.random_state=%4 ^
 model=%3 ^
 evaluation=ml.yaml ^
