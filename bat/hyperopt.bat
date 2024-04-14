68ad37a5c7a38d9f8a9836c33876bfe866450570
Alexander Grote <grote@fzi.de>
2024-04-14 15:55:52 +0200

    adding random state to bat script

--- bat/hyperopt.bat
+++ bat/hyperopt.bat
@@ -8,7 +8,7 @@ for %%f in ("synthetic", "malware", "churn") do (
         constants=%%~f ^
         fetch_data=%%~f ^
         preprocess=%%~s ^
-        train_test_split=stratified ^
+        train_test_split=stratified train_test_split.params.random_state=0 ^
         model=xgb_tuned ^
         evaluation=ml.yaml ^
         export=mlflow.yaml export.params.experiment_name="hyper_opt__"%%~f"__"%%~s
